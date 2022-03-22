7bb39a08077ec0995eb42f3493cb503362634f37
Nika <nikakakhniashvili1@gmail.com>
2022-03-22 18:01:55 +0400

    added front changes

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from polls.views import hungry
+from polls.views import gshia,gamodzaxeba
 
 urlpatterns = [
 
-    path('poll1/', hungry, name='poll1' )
+    path('poll1/', gshia, name='poll1'),
+    path('poll2/', gamodzaxeba, name='poll2'),
+#    path('poll3/' reschoose, name='poll3')
 
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 
 
 
 
 @login_required
-def hungry(request):
+def gshia(request):
+    if request.method == 'POST':
+        if request.POST.get("action") == 'yes':
+            return redirect('poll2')    
+        else:
+            return redirect('home')
     return render(request, "poll/poll1.html", {})
+
+@login_required
+def gamodzaxeba(request):
+    if request.method == 'POST':
+        return redirect('poll3')
+    
+    return render(request, 'poll/poll2.html', {})
